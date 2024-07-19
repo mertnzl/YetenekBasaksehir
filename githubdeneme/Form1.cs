2f6b93270a45555ac04a6936f7d7b6f9cd334912
Mert Nazlı <mertnazli34@outlook.com>
2024-07-19 10:13:14 +0300

    Mesaj box eklendi

--- githubdeneme/Form1.cs
+++ githubdeneme/Form1.cs
@@ -6,5 +6,10 @@ namespace githubdeneme
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("sdlfksdlþ");
+        }
     }
 }
